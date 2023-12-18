d91158a89c4f5cc2f88b66c64f0a1aac5afaeeec
bingnuoer <2366858264@qq.com>
2023-12-18 10:31:32 +0800

    设置请求拦截器，请求个人信息并设置到页面

--- utils/auth.js
+++ utils/auth.js
@@ -4,6 +4,7 @@
  * 1.1 判断无 token 令牌字符串，则强制跳转到登录页
  * 1.2 登录成功后，保存 token 令牌字符串到本地，并跳转到内容列表页面
  */
+
 // 1.1 判断无 token 令牌字符串，则强制跳转到登录页
 const token = localStorage.getItem('token')
 if (!token) {
@@ -16,6 +17,20 @@ if (!token) {
  * 2.1 在 utils/request.js 设置请求拦截器，统一携带 token
  * 2.2 请求个人信息并设置到页面
  */
+// 2.2请求个人信息并设置到页面
+axios({
+    url: '/v1_0/user/profile'
+    // Header请求头参数在 utils/request.js 设置请求拦截器配置好
+}).then(result => {
+    console.log(result);
+
+    // 请求个人信息并设置到页面
+    document.querySelector('.nick-name').innerHTML = result.data.data.name
+    
+}).catch(error => {
+    console.log(error);
+})
+
 
 /**
  * 目标3：退出登录
--- utils/request.js
+++ utils/request.js
@@ -1,3 +1,17 @@
 // axios 公共配置
 // 基地址
 axios.defaults.baseURL = 'http://geek.itheima.net'
+
+// 添加请求拦截器-axios官网copy
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    // 设置请求拦截器，统一携带 token
+    const token = localStorage.getItem('token')
+    // 请求头参数
+    token && (config.headers.Authorization = `Bearer ${token}`)
+    
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });

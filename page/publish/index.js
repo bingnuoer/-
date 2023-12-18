548a4990859ed479d53575c93feb3b1b03c06da8
bingnuoer <2366858264@qq.com>
2023-12-18 15:03:52 +0800

    频道下拉菜单与文章封面设置

--- page/publish/index.js
+++ page/publish/index.js
@@ -4,6 +4,27 @@
  *  1.2 展示到下拉菜单中
  */
 
+// 封装函数，复用
+async function getChannleList() {
+    // 1.1 发送请求，获取频道列表数据
+    const res = await axios({
+        url: '/v1_0/channels'
+    })
+
+    // console.log("频道列表数据");
+    // console.log(res);
+
+    // 数组元素项转换标签
+    const htmlStr = `<option value="" selected="">请选择文章频道</option>` + res.data.channels.map(item => {
+        return `<option value="${item.id}">${item.name}</option>`
+    }).join('')
+    // console.log(htmlStr);
+    // 渲染到页面标签
+    document.querySelector('.form-select').innerHTML = htmlStr
+}
+// 打开页面，默认调用一次
+getChannleList()
+
 /**
  * 目标2：文章封面设置
  *  2.1 准备标签结构和样式
@@ -11,6 +32,39 @@
  *  2.3 单独上传图片并得到图片 URL 网址
  *  2.4 回显并切换 img 标签展示（隐藏 + 号上传标签）
  */
+// 2.2 选择文件并保存在 FormData
+// 监听图片选择区域的change事件
+document.querySelector('.img-file').addEventListener('change', async e => {
+    // 将用户选择的图片文件转换为 FormData 格式，并将其作为请求体发送到服务器
+    const file = e.target.files[0]
+    // 图片转换成FormData格式，在发送请求
+    const fd = new FormData()
+    // 将选择的文件添加到 FormData 实例中，并指定表单数据的键名为 'img'。也就是说，在后续的请求中，服务器端可以通过这个键名来获取上传的文件。
+    fd.append('image', file)
+
+    // 2.3 单独上传图片并得到图片 URL 网址
+    const res = await axios({
+        url: '/v1_0/upload',
+        method: 'POST',
+        data: fd
+    })
+    console.log(res);
+    // 获取上传的图片url
+    const imgUrl = res.data.url
+    // 渲染到页面img标签中
+    document.querySelector('.rounded').src = imgUrl
+    // 显示img标签
+    document.querySelector('.rounded').classList.add('show')
+    // 隐藏上传图片的“+”标签
+    document.querySelector('.place').classList.add('hide')
+})
+
+// 优化：点击 img 可以重新切换封面
+// 思路：img点击 => 用JS方式触发文件选择元素click事件方法
+document.querySelector('.rounded ').addEventListener('click',()=> {
+    document.querySelector('.img-file').click()
+    // 循环->监听图片选择区域的change事件-重新覆盖一张封面
+})
 
 /**
  * 目标3：发布文章保存

e6f0e61ca07ca767a4fb78608e46ede92784270e
bingnuoer <2366858264@qq.com>
2023-12-18 10:10:33 +0800

    搭建内容详情页面。登录成功跳转内容详情页

--- page/login/index.js
+++ page/login/index.js
@@ -5,3 +5,40 @@
  * 1.3 基于 axios 调用验证码登录接口
  * 1.4 使用 Bootstrap 的 Alert 警告框反馈结果给用户
  */
+
+// 点击登录
+document.querySelector('.btn').addEventListener('click', () => {
+    // 1.2 收集手机号和验证码数据
+    // serialize获取表单数据，表单必须有name属性
+    const form = document.querySelector('.login-form')
+    const data = serialize(form, { harsh: true, empty: true })
+    console.log("表单数据" + data);
+    console.log(data.mobile);
+
+    // 发起请求，登录
+    axios({
+        url: '/v1_0/authorizations',
+        method: 'POST',
+        data
+    }).then(result => {
+        console.log(result);
+        // 调用bootstrap设置弹框
+        myAlert(true, '登录成功！')
+
+        // 登录成功后，保存 token 令牌字符串到本地，并跳转到内容列表页面
+        localStorage.setItem('token', result.data.data.token)
+
+        // 弹框弹出后，延迟1.5秒后执行
+        setTimeout(() => {
+            // 跳转到内容列表页面
+            location.href = '../content/index.html'
+        }, 1500)
+
+
+    }).catch(error => {
+        console.log(error);
+        // alert(error.response.data.message)
+
+        myAlert(false, error.response.data.message)
+    })
+})
--- utils/auth.js
+++ utils/auth.js
@@ -4,6 +4,12 @@
  * 1.1 判断无 token 令牌字符串，则强制跳转到登录页
  * 1.2 登录成功后，保存 token 令牌字符串到本地，并跳转到内容列表页面
  */
+// 1.1 判断无 token 令牌字符串，则强制跳转到登录页
+const token = localStorage.getItem('token')
+if (!token) {
+    location.href = '../login/index.html'
+}
+
 
 /**
  * 目标2：设置个人信息
